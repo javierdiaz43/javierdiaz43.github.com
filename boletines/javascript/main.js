ba80f883d997e2444abdac20ec4b363ec4eb82ef
javierdiaz43 <javierdiaz43@users.noreply.github.com>
2020-04-03 04:41:17 -0300

    Correción de errores de tipeo

--- boletines/javascript/main.js
+++ boletines/javascript/main.js
@@ -1,19 +1,20 @@
 //obtener los elementos de la clase .clase
 let links = document.querySelectorAll(".close");
 //recorrelos
-links.forEach(function(link)){
+links.forEach(function(link){
   //agregar un evento click a cada uno de ellos
   link.addEventListener("click",function(ev){
     ev.preventDefault();
-    let content = document.querySelector(".cotent");
+    let content = document.querySelector(".content");
     //quitarle las clases de animacion que ya addEventListener
     content.classList.remove("fadeInDown");
     content.classList.remove("animated");
     //Agregar clases para animar la salida "fadeOutUp"
     content.classList.add("fadeOutUp");
     content.classList.add("animated");
-    setTimeout(function){  // genera un delay de 600 milisegundos en la ejecucion de la sig. instrucción
-      location.ref = "/";  //no devuelve a la pagina inicial
+
+    setTimeout(function(){  // genera un delay de 600 milisegundos en la ejecucion de la sig. instrucción
+      location.href = "/";  //no devuelve a la pagina inicial
     }, 600)
     return false;
   });
