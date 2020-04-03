05385a646867149a11fe09e0a720b985c8fed229
javierdiaz43 <javierdiaz43@users.noreply.github.com>
2020-04-03 03:56:32 -0300

    Se cambia animacion de salida

--- boletines/javascript/main.js
+++ boletines/javascript/main.js
@@ -1,8 +1,19 @@
-//DOM
-//querySelector                               selecciona el primer elemento encontrado.
-/*
-let link = document.querySelectorAll("a");   asigna a una lsita todos los elemntos "a" encontrados
-links.forEach((function(link){               funcion que recoore el arreglo e imprime en cosola los elementos encontrados
-  console.log(link);
+//obtener los elementos de la clase .clase
+let links = document.querySelectorAll(".close");
+//recorrelos
+links.forEach(function(link)){
+  //agregar un evento click a cada uno de ellos
+  link.addEventListener("click",function(ev){
+    ev.preventDefault();
+    let content = document.querySelector(".cotent");
+    //quitarle las clases de animacion que ya addEventListener
+    content.classList.remove("fadeInDown");
+    content.classList.remove("animated");
+    //Agregar clases para animar la salida "fadeOutUp"
+    content.classList.add("fadeOutUp");
+    content.classList.add("animated");
+
+    location.ref = "/";
+    return false;
+  });
 });
-*/
