e6117035e6cf61dc845da67e52047b1951dfe530
javierdiaz43 <javierdiaz43@users.noreply.github.com>
2020-04-03 02:37:48 -0300

    primeros ejercicios

---
+++ boletines/javascript/main.js
@@ -0,0 +1,8 @@
+//DOM
+//querySelector                               selecciona el primer elemento encontrado.
+/*
+let link = document.querySelectorAll("a");   asigna a una lsita todos los elemntos "a" encontrados
+links.forEach((function(link){               funcion que recoore el arreglo e imprime en cosola los elementos encontrados
+  console.log(link);
+});
+*/
